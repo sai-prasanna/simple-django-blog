75e469add074a900d6c8252cce5e4acd73a395c4
Sai Prasanna <sai.r.prasanna@gmail.com>
2014-10-09 20:58:42 +0530

    remove print statement

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    print(sys.path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.local")
 
     from django.core.management import execute_from_command_line
